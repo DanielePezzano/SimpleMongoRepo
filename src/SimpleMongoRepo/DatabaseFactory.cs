0bce4c22774f640d9032122309a6982a75aedddf
Daniele <daniele.pezzano.consultant@yoox.com>
2018-01-19 11:13:45 +0100

    factory implemented

---
+++ src/SimpleMongoRepo/DatabaseFactory.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+using MongoDB.Driver;
+
+namespace SimpleMongoRepo
+{
+    public static class DatabaseFactory
+    {
+        private static IMongoClient CreateClient(string connectionString)
+        {
+            return new MongoClient(connectionString);
+        }
+
+        public static IMongoDatabase Create(string connectionString)
+        {
+            var client = CreateClient(connectionString);
+            var dbName = Regex.Match(connectionString, @"(\w*)\?").Groups[1].ToString();
+            return client.GetDatabase(dbName);
+        }
+    }
+}
---
+++ src/SimpleMongoRepo/RepositoryFactory.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Driver;
+using SimpleMongoRepo.Interfaces;
+
+namespace SimpleMongoRepo
+{
+    public static class RepositoryFactory<T> where T : IMongoEntity
+    {
+        public static Repository<T> GetRepository(IMongoDatabase database, string collection)
+        {
+            return new Repository<T>(database, collection);
+        }
+    }
+}
